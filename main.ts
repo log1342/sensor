8c329b173b2a4d32403970fde8187a0e59edd45d
log1342 <113910996+log1342@users.noreply.github.com>
2022-12-09 09:30:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,21 +2,27 @@ input.onButtonPressed(Button.A, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
-    while (true) {
-        sensor()
-        basic.pause(2000)
-    }
+    sensor()
 })
 function sensor () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    basic.showString("" + (distance))
+    counter = 0
+    for (let index = 0; index < 10; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+        if (distance <= 5) {
+            counter += 1
+        }
+    }
+    if (counter >= 5) {
+        basic.showIcon(IconNames.Yes)
+    }
 }
 let distance = 0
+let counter = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
